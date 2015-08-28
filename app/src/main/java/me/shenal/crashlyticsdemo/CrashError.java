3a06a733e5661f9ad1ad019843c69cb912e1e8d4
Shenal Silva <shenal.silva007@gmail.com>
2015-08-28 12:02:43 +0530

    Initial commit

---
+++ app/src/main/java/me/shenal/crashlyticsdemo/CrashError.java
@@ -0,0 +1,12 @@
+package me.shenal.crashlyticsdemo;
+
+/**
+ * Created by shenal on 8/26/15.
+ */
+public class CrashError extends RuntimeException {
+    public  static void error() {
+
+    }
+
+
+}
